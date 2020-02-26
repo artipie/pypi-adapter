a6df883aa0d76471a9d66503b103d2a7beaa6ae6
guseyn <guseynism@gmail.com>
2020-02-26 14:58:02 +0300

    reesolve #28: serve static files via py slice

---
+++ src/main/java/com/artipie/pypi/StaticContent.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.artipie.pypi;
+
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.http.Response;
+import com.artipie.http.rs.RsWithStatus;
+import java.net.HttpURLConnection;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * StaticContent.
+ *
+ * @since 0.1
+ */
+public final class StaticContent implements Resource {
+    /**
+     * Resource path.
+     */
+    private final String path;
+
+    /**
+     * Storage to read content from.
+     */
+    private final Storage storage;
+
+    /**
+     * Ctor.
+     *
+     * @param path Resource path.
+     * @param storage Storage to read content from.
+     */
+    public StaticContent(final String path, final Storage storage) {
+        this.path = path;
+        this.storage = storage;
+    }
+
+    @Override
+    public Response get() {
+        Response response;
+        try {
+            response = this.getAsync().get();
+        } catch (final InterruptedException | ExecutionException ex) {
+            response = new RsWithStatus(HttpURLConnection.HTTP_INTERNAL_ERROR);
+        }
+        return response;
+    }
+
+    /**
+     * Serve GET method async.
+     *
+     * @return Response to request.
+     */
+    private Future<Response> getAsync() {
+        return CompletableFuture
+            .supplyAsync(() -> new Key.From(this.path))
+            .thenCompose(
+                key -> this.storage.exists(key).thenCompose(
+                    exists -> {
+                        final CompletionStage<Response> response;
+                        if (exists) {
+                            response = this.storage.value(key).thenApplyAsync(
+                                data -> connection -> connection.accept(
+                                    HttpURLConnection.HTTP_OK,
+                                    Collections.emptyList(),
+                                    data
+                                )
+                            );
+                        } else {
+                            response = CompletableFuture.completedFuture(
+                                new RsWithStatus(HttpURLConnection.HTTP_NOT_FOUND)
+                            );
+                        }
+                        return response;
+                    }
+                )
+            );
+    }
+}
---
+++ src/test/java/com/artipie/pypi/PyGetTest.java
@@ -0,0 +1,125 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.artipie.pypi;
+
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.asto.blocking.BlockingStorage;
+import com.artipie.asto.fs.FileStorage;
+import com.artipie.http.Response;
+import com.artipie.http.hm.RsHasBody;
+import com.artipie.http.hm.RsHasStatus;
+import io.reactivex.Flowable;
+import java.net.HttpURLConnection;
+import java.nio.file.Path;
+import java.util.Collections;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+/**
+ * PyGetTest.
+ *
+ * @since 0.1
+ */
+class PyGetTest {
+
+    /**
+     * Storage used in tests.
+     */
+    private Storage storage;
+
+    /**
+     * Tested Py slice.
+     */
+    private PySlice pyslice;
+
+    @BeforeEach
+    void init(final @TempDir Path temp) {
+        this.storage = new FileStorage(temp);
+        this.pyslice = new PySlice("/base/", this.storage);
+    }
+
+    @Test
+    void shouldGetPackageContent() {
+        final byte[] data = "data".getBytes();
+        new BlockingStorage(this.storage).save(
+            new Key.From("package", "1.0.0", "content.py"),
+            data
+        );
+        final Response response = this.pyslice.response(
+            "GET /base/package/1.0.0/content.py",
+            Collections.emptyList(),
+            Flowable.empty()
+        );
+        MatcherAssert.assertThat(
+            response,
+            Matchers.allOf(
+                new RsHasStatus(HttpURLConnection.HTTP_OK),
+                new RsHasBody(data)
+            )
+        );
+    }
+
+    @Test
+    void shouldFailGetPackageContentFromNotBasePath() {
+        final Response response = this.pyslice.response(
+            "GET /not-base/package/1.0.0/content.py",
+            Collections.emptyList(),
+            Flowable.empty()
+        );
+        MatcherAssert.assertThat(
+            "Resources from outside of base path should not be found",
+            response,
+            new RsHasStatus(HttpURLConnection.HTTP_NOT_FOUND)
+        );
+    }
+
+    @Test
+    void shouldFailGetPackageContentWhenNotExists() {
+        final Response response = this.pyslice.response(
+            "GET /base/package/1.0.0/logo.png",
+            Collections.emptyList(),
+            Flowable.empty()
+        );
+        MatcherAssert.assertThat(response, new RsHasStatus(HttpURLConnection.HTTP_NOT_FOUND));
+    }
+
+    @Test
+    void shouldFailPutPackageContent() {
+        final Response response = this.pyslice.response(
+            "PUT /base/package/1.0.0/content.py",
+            Collections.emptyList(),
+            Flowable.empty()
+        );
+        MatcherAssert.assertThat(
+            "Package content cannot be put",
+            response,
+            new RsHasStatus(HttpURLConnection.HTTP_BAD_METHOD)
+        );
+    }
+}
