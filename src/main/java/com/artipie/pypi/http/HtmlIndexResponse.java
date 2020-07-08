df614a882492041bc95993e2b35400ccc4ddf374
olenagerasimova <olena.gerasimova@gmail.com>
2020-07-08 17:21:16 +0300

    Updated http to 0.13

--- src/main/java/com/artipie/pypi/http/HtmlIndexResponse.java
+++ src/main/java/com/artipie/pypi/http/HtmlIndexResponse.java
@@ -26,7 +26,7 @@ package com.artipie.pypi.http;
 
 import com.artipie.http.Connection;
 import com.artipie.http.Response;
-import com.artipie.http.rs.Header;
+import com.artipie.http.headers.Header;
 import com.artipie.http.rs.RsStatus;
 import com.artipie.http.rs.RsWithBody;
 import com.artipie.http.rs.RsWithHeaders;
