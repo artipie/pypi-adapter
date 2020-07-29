9f74541adfe15b38a7868750d584a2b330e5ef43
olenagerasimova <olena.gerasimova@gmail.com>
2020-07-29 12:10:54 +0300

    #86 - read project name and version from metadata

---
+++ src/main/java/com/artipie/pypi/meta/PackageInfo.java
@@ -0,0 +1,105 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.pypi.meta;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import org.cactoos.io.ReaderOf;
+
+/**
+ * Python package info.
+ * @since 0.6
+ */
+public interface PackageInfo {
+
+    /**
+     * Package name.
+     * @return Name of the project
+     */
+    String name();
+
+    /**
+     * Package version.
+     * @return Version of the project
+     */
+    String version();
+
+    /**
+     * Implementation of {@link PackageInfo} that parses python metadata PKG-INFO file to obtain
+     * required information. For more details see
+     * <a href="https://www.python.org/dev/peps/pep-0314/">PEP-314</a>.
+     * @since 0.6
+     */
+    final class FromMetadata implements PackageInfo {
+
+        /**
+         * Input.
+         */
+        private final InputStream input;
+
+        /**
+         * Ctor.
+         * @param input Input
+         */
+        public FromMetadata(final InputStream input) {
+            this.input = input;
+        }
+
+        @Override
+        public String name() {
+            return this.read("Name");
+        }
+
+        @Override
+        public String version() {
+            return this.read("Version");
+        }
+
+        /**
+         * Reads header value by name.
+         * @param header Header name
+         * @return Header value
+         */
+        @SuppressWarnings("PMD.AssignmentInOperand")
+        private String read(final String header) {
+            try (BufferedReader br = new BufferedReader(new ReaderOf(this.input))) {
+                final String name = String.format("%s:", header);
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (line.startsWith(name)) {
+                        this.input.reset();
+                        return line.replace(name, "").trim();
+                    }
+                }
+                throw new IllegalArgumentException(
+                    String.format("Invalid metadata file, header %s not found", header)
+                );
+            } catch (final IOException ex) {
+                throw new UncheckedIOException(ex);
+            }
+        }
+    }
+}
